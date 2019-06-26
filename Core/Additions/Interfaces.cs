fc3f568e90f8c44f8a78fd8a45db0e70113f7ff2
Johan Driessen <johan@driessen.se>
2019-06-26 13:29:05 +0200

    Added rules for Core

---
+++ Core/Additions/Interfaces.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//These interfaces were missing after the code generation.
+//Maybe they need some methods too?
+
+namespace Com.Adyen.Checkout.Core.Internal.Persistence
+{
+    public interface IPaymentSessionDao : global::Android.Runtime.IJavaObject
+	{
+
+    }
+
+	public interface IPaymentInitiationResponseDao : global::Android.Runtime.IJavaObject
+	{
+
+	}
+}
